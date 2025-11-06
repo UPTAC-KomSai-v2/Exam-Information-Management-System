b94582a4f4e28f607f13da57b7ba33462906ef54
nikkanap <nenaputo@up.edu.ph>
2025-11-06 22:03:15 +0800

    Add all Next.js files

---
+++ src/app/page.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Nav from "./components/nav";
+import Link from 'next/link';
+
+let logo = "/images/logo.png";
+let desc1 = "The premier Examination Information Management System for the University of the Philippines.";
+let desc2 = "Maroon Book can serve as the main platform for managing all of your class examinations.";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Nav />
+
+      <main className={styles.main}>
+        <div className={styles.intro}>
+          <Image
+            src={logo}
+            alt="Logo"
+            width={500}
+            height={104}
+          />
+          <h1>{desc1}</h1>
+          <p>{desc2}</p>
+        </div>
+
+        <div className={styles.buttons}>
+          <a
+            className={styles.primary}
+            href="/dashboard"
+            rel="noopener noreferrer"
+          >
+            Register an Account
+          </a>
+
+          <a
+            className={styles.primary}
+            href="/dashboard"
+            rel="noopener noreferrer"
+          >
+            Login to an Account
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
