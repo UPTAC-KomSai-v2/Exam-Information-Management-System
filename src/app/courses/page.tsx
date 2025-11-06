95d9cc415a31ec7dfdce43d70030624913945554
nikkanap <nenaputo@up.edu.ph>
2025-11-06 22:41:16 +0800

    Create Courses page and move "enroll course" UI to Courses

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -20,17 +20,12 @@ export default function Home() {
     <div className={styles.page}>
       <Nav />
       <main className={styles.main}>
-        <div className={styles.examDiv}>
-          <p className={styles.title}>Second Long Exam (Lecture)</p>
-          <p className={styles.description}>CMSC 135 - F | Data Communication and Networking | AY 2026 - 2026 First Semester</p>
+        <div className={styles.courseDiv}>
+          <p className={styles.title}>CMSC 135 - F</p>
+          <p className={styles.description}>Data Communication and Networking | AY 2026 - 2026 First Semester</p>
           <div className={styles.information}>
-            <p>Total Items: 50</p>
-            <p>Grade: N/A</p>
-            <p>Exam Type: Submission</p>
-            <p>Time Allotted: 30 minutes</p>
-            <p>Due Date: November 27, 2025</p>
-            <p>Taken: Not yet</p>
-            <a>Take Exam</a>
+            <p>Total Exams: 2</p>
+            <a>View Course Report</a>
           </div>
         </div>
 
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -33,49 +33,6 @@ export default function Home() {
             <a>Take Exam</a>
           </div>
         </div>
-
-        <button
-          className={styles.enrollCourse}
-          onClick={() => setShowOverlay(true)}
-        >
-          +
-        </button>
-
-        { showOverlay && (
-          <div className={styles.filter}>
-          </div>
-        )}
-        {
-          showOverlay && (
-          <div className={styles.overlay}>
-            <Image
-              src={logo}
-              alt="Logo"
-              width={217}
-              height={26}
-            />
-            <p className={styles.overlayTitle}>Enroll a course</p>
-            
-            <div>
-              <p>To enroll on a course, please</p>
-              <p> enter the course code below.</p>
-            </div>
-            
-            <div className={styles.inputContainer}>
-              <label>Course Code:</label>
-              <input type="text" name="courseCode" className={styles.courseCode}/>
-            </div>
-            <button 
-              className={styles.submit}
-              onClick={() => setShowOverlay(false)}
-            >Submit</button>
-            <button 
-              className={styles.exitOverlay} 
-              onClick={() => setShowOverlay(false)}
-            >✖</button>
-          </div>
-        )}
-
       </main>
     </div>
   );
