c0adca0191df2e9e010f218c834e4a1703f48004
nikkanap <nenaputo@up.edu.ph>
2025-11-30 23:19:34 +0800

    Remove dash from student and employee numbers

---
+++ src/app/user/employee/createExam/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { courses, referenceExams, type ReferenceExam } from "~/app/data/data";
+import mainStyle from "./page.module.css";
+import styles from "~/app/user/components/shared.module.css";
+import Nav from "~/app/user/components/userNav";
+import { type ReactNode, useContext } from "react";
+import { type Employee, UserContext } from "~/app/UserContext";
+import { LinkButton } from "~/app/_components/links";
+
+export default function createExam() {
+    const { currentUser } = useContext(UserContext);
+
+    if (!currentUser) return <p>No user is logged in</p>;
+    if (currentUser.type !== "employee") return <p>User logged in is not employee</p>;
+
+    return (
+        <div className={styles.page}>
+            <Nav scope="employee" />
+
+            <main className={`${styles.main} ${mainStyle.main} main `}>
+                <div className={styles.upperDiv}>
+                    <p className="title22px">Dashboard</p>
+                    <LinkButton href="/createExam" className="primaryButton">
+                        Create New Exam
+                    </LinkButton>
+                </div>
+                <RenderExamList currentUser={currentUser} />
+            </main>
+        </div>
+    );
+}
+
+function RenderExamList({ currentUser }: { currentUser: Employee }) {
+    const indivExamContent = (refExam: ReferenceExam, examDescription: string, sectionNames: string) => {
+        return(
+            <div className={styles.examCourseDiv} key={refExam.examID}>
+                <p className="title22px">{refExam.examTitle}</p>
+                <p className={styles.description}>{examDescription}</p>
+                <div className={styles.information}>
+                    <p>Sections: {sectionNames}</p>
+                    <p>Total Items: {refExam.items}</p>
+                    <p>Exam Type: {refExam.examType}</p>
+                    <p>Time Allotted: {refExam.timeAllotted}</p>
+                    <p>Due Date: {refExam.dueDate}</p>
+                    <a>Publish Exam</a>
+                    <a>Hide Exam</a>
+                    <LinkButton href="#" className="">View Exam</LinkButton>
+                </div>
+            </div>
+        );
+    }
+
+    const examList: ReactNode[] = [];
+    const coursesTaught = courses.filter(course => course.courseEmployeeID === currentUser.userID);
+
+    coursesTaught.forEach((course) => {
+        const refExams = referenceExams.filter(refExam => refExam.courseID === course.courseID);
+        
+        let sectionNames = "";
+        course.sections.forEach(section => sectionNames += `${section.sectionName} , `);
+        sectionNames.slice(0, sectionNames.length-2);
+
+        refExams.forEach((refExam) => {
+            const courseDescription = `${course.courseTitle} | ${course.courseDescription}`
+            examList.push(indivExamContent(refExam, courseDescription, sectionNames));
+        });
+    });
+
+    return examList;
+}
